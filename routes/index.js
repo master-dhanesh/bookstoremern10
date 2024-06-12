5eaf7d34281ef22a592d4407980f88973f9f791d
master-dhanesh <dhanesh1296@gmail.com>
2024-06-12 10:10:16 +0530

    Filefilter Multer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const BookCollection = require("../models/bookSchema");
 const { checkPrice } = require("../utils/middlewares");
-const upload = require("../utils/multer");
+const upload = require("../utils/multer").single("poster");
 
 router.get("/", function (req, res, next) {
     res.render("home");
@@ -27,24 +27,50 @@ router.get("/create-book", function (req, res, next) {
     res.render("createbook");
 });
 
-router.post(
-    "/create-book",
-    upload.single("poster"),
-    checkPrice,
-    async function (req, res, next) {
-        try {
-            const newBook = await new BookCollection({
-                ...req.body,
-                poster: req.file.filename,
-            });
+// router.post(
+//     "/create-book",
+//     upload.single("poster"),
+//     checkPrice,
+//     async function (req, res, next) {
+//         try {
+//             const newBook = await new BookCollection({
+//                 ...req.body,
+//                 poster: req.file.filename,
+//             });
 
-            await newBook.save();
-            res.redirect("/library");
-        } catch (error) {
-            res.send(error);
+//             await newBook.save();
+//             res.redirect("/library");
+//         } catch (error) {
+//             console.log(error);
+//             res.send(error);
+//         }
+//     }
+// );
+
+router.post("/create-book", checkPrice, async function (req, res, next) {
+    upload(req, res, async function (err) {
+        if (err) {
+            console.log(err);
+            res.json(err);
+        } else {
+            // ------------------------
+            try {
+                const newBook = await new BookCollection({
+                    ...req.body,
+                    poster: req.file.filename,
+                });
+
+                await newBook.save();
+                res.redirect("/library");
+            } catch (error) {
+                console.log(error);
+                res.send(error);
+            }
+
+            // --------------------
         }
-    }
-);
+    });
+});
 
 router.get("/details/:id", async function (req, res, next) {
     try {
--- utils/multer.js
+++ utils/multer.js
@@ -2,16 +2,32 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
+    destination: function (req, file, cb) {
         cb(null, "public/images");
     },
-    filename: (req, file, cb) => {
-        const modifiedFilename = Date.now() + path.extname(file.originalname);
-
-        cb(null, modifiedFilename);
+    filename: function (req, file, cb) {
+        const updatedName = Date.now() + path.extname(file.originalname);
+        cb(null, updatedName);
     },
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png|gif|svg|webp/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+    );
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb("Error: Images Only!");
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+});
 
 module.exports = upload;
