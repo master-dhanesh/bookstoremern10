a7c2e38ce33b24d1ee771c58018b56c12f17f6ce
master-dhanesh <dhanesh1296@gmail.com>
2024-06-12 10:17:42 +0530

    deleting data with image in public/images

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var router = express.Router();
+const path = require("path");
+const fs = require("fs");
 
 const BookCollection = require("../models/bookSchema");
 const { checkPrice } = require("../utils/middlewares");
-const upload = require("../utils/multer").single("poster");
+const upload = require("../utils/multer");
 
 router.get("/", function (req, res, next) {
     res.render("home");
@@ -27,50 +29,50 @@ router.get("/create-book", function (req, res, next) {
     res.render("createbook");
 });
 
-// router.post(
-//     "/create-book",
-//     upload.single("poster"),
-//     checkPrice,
-//     async function (req, res, next) {
-//         try {
-//             const newBook = await new BookCollection({
-//                 ...req.body,
-//                 poster: req.file.filename,
-//             });
+router.post(
+    "/create-book",
+    upload.single("poster"),
+    checkPrice,
+    async function (req, res, next) {
+        try {
+            const newBook = await new BookCollection({
+                ...req.body,
+                poster: req.file.filename,
+            });
 
-//             await newBook.save();
-//             res.redirect("/library");
-//         } catch (error) {
-//             console.log(error);
-//             res.send(error);
-//         }
-//     }
-// );
-
-router.post("/create-book", checkPrice, async function (req, res, next) {
-    upload(req, res, async function (err) {
-        if (err) {
-            console.log(err);
-            res.json(err);
-        } else {
-            // ------------------------
-            try {
-                const newBook = await new BookCollection({
-                    ...req.body,
-                    poster: req.file.filename,
-                });
-
-                await newBook.save();
-                res.redirect("/library");
-            } catch (error) {
-                console.log(error);
-                res.send(error);
-            }
-
-            // --------------------
+            await newBook.save();
+            res.redirect("/library");
+        } catch (error) {
+            console.log(error);
+            res.send(error);
         }
-    });
-});
+    }
+);
+
+// router.post("/create-book", checkPrice, async function (req, res, next) {
+//     upload(req, res, async function (err) {
+//         if (err) {
+//             console.log(err);
+//             res.json(err);
+//         } else {
+//             // ------------------------
+//             try {
+//                 const newBook = await new BookCollection({
+//                     ...req.body,
+//                     poster: req.file.filename,
+//                 });
+
+//                 await newBook.save();
+//                 res.redirect("/library");
+//             } catch (error) {
+//                 console.log(error);
+//                 res.send(error);
+//             }
+
+//             // --------------------
+//         }
+//     });
+// });
 
 router.get("/details/:id", async function (req, res, next) {
     try {
@@ -104,7 +106,10 @@ router.post("/update-book/:id", async function (req, res, next) {
 
 router.get("/delete-book/:id", async function (req, res, next) {
     try {
-        await BookCollection.findByIdAndDelete(req.params.id);
+        const book = await BookCollection.findByIdAndDelete(req.params.id);
+
+        fs.unlinkSync(path.join(__dirname, `../public/images/${book.poster}`));
+
         res.redirect("/library");
     } catch (error) {
         console.log(error);
